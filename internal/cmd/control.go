ab1d1b63b756497b34f49b6dc0fc5caaa95cce1e
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-06 15:06:49 +0200

    add internal/cmd/control.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/control.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"log"
+	"net"
+)
+
+// control stores control server information
+type control struct {
+	addr     *net.TCPAddr
+	listener *net.TCPListener
+}
+
+// runControl runs the control server
+func (c *control) runControl() {
+	// start control socket
+	listener, err := net.ListenTCP("tcp", c.addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	c.listener = listener
+	for {
+		// get new control connection
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// TODO: do something with conn
+		conn.Close()
+	}
+}
+
+// runControl runs the control server on addr
+func runControl(addr *net.TCPAddr) {
+	// create control server
+	c := control{
+		addr: addr,
+	}
+
+	// run control server
+	c.runControl()
+}
