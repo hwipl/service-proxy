85a108a351d51068e143ea66ba0661097c9cef89
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-06 15:09:27 +0200

    run control server on startup
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,5 +1,13 @@
 package cmd
 
+import "net"
+
 // Run is the main entry point
 func Run() {
+	// run control server
+	cntrlAddr := net.TCPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 32323,
+	}
+	runControl(&cntrlAddr)
 }
