cf465eee06fdd5e1d490ae13e204e48b218c9a68
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-06 15:59:27 +0200

    add internal/cmd/message.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/message.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+)
+
+// message stores a control message
+type message struct {
+	op       uint8
+	protocol uint8
+	port     uint16
+	destPort uint16
+}
+
+// serialize writes message to a byte slice
+func (m *message) serialize() []byte {
+	var buf bytes.Buffer
+	err := binary.Write(&buf, binary.BigEndian, m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return buf.Bytes()
+}
+
+// parse reads message from byte slice b
+func (m *message) parse(b []byte) {
+	buf := bytes.NewBuffer(b)
+	err := binary.Read(buf, binary.BigEndian, m)
+	if err != nil {
+		fmt.Println("error reading message:", err)
+	}
+}
