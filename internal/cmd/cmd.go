0b0c7f7d52ed27b773ea1c8cbad8e13040818cfa
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-06 16:22:37 +0200

    start a service proxy on startup for testing
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -4,6 +4,17 @@ import "net"
 
 // Run is the main entry point
 func Run() {
+	// test service proxying
+	srvAddr := net.TCPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 33000,
+	}
+	dstAddr := net.TCPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 8000,
+	}
+	runTCPService(&srvAddr, &dstAddr)
+
 	// run control server
 	cntrlAddr := net.TCPAddr{
 		IP:   net.IPv4(0, 0, 0, 0),
