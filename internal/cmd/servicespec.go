ac5bcc885a5fda94ab7c90f3080cbaa1cef3aff3
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-10 13:26:58 +0200

    add internal/cmd/servicespec.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/servicespec.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+// serviceSpec stores the specification of a service
+type serviceSpec struct {
+	protocol string
+	port     uint16
+	destPort uint16
+}
+
+// parseServiceSpec parses spec as a service specification with the format
+// "<protocol>:<port>:<destPort>"
+func parseServiceSpec(spec string) *serviceSpec {
+	errFmt := "Error parsing service specification %s"
+	parts := strings.Split(spec, ":")
+	if len(parts) != 3 {
+		log.Fatalf(errFmt, spec)
+	}
+
+	// parse protocol
+	protocol := parts[0]
+
+	// parse port
+	port, err := strconv.ParseInt(parts[1], 10, 16)
+	if err != nil {
+		log.Fatalf(errFmt, spec)
+	}
+
+	// parse destination port
+	destPort, err := strconv.ParseUint(parts[2], 10, 16)
+	if err != nil {
+		log.Fatalf(errFmt, spec)
+	}
+
+	// return as serviceSpec
+	s := serviceSpec{
+		protocol: protocol,
+		port:     uint16(port),
+		destPort: uint16(destPort),
+	}
+	return &s
+}
