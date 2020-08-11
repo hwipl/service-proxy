24d289db4ac6b547c515a4ef6465b8d5dc7ebe85
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-11 12:44:28 +0200

    forward traffic from destination to peer in udpForwarder
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/udpforwarder.go
+++ internal/cmd/udpforwarder.go
@@ -7,9 +7,23 @@ import "net"
 type udpForwarder struct {
 	srvConn *net.UDPConn
 	dstConn *net.UDPConn
+	peer    *net.UDPAddr
 }
 
 // runForwarder runs the udp forwarder
 func (u *udpForwarder) runForwarder() {
-	// not implemented
+	defer u.dstConn.Close()
+	for {
+		// read packets from proxy destination and send them to the
+		// service peer
+		buf := make([]byte, 2048)
+		n, err := u.dstConn.Read(buf)
+		if err != nil {
+			return
+		}
+		_, err = u.srvConn.WriteToUDP(buf[:n], u.peer)
+		if err != nil {
+			return
+		}
+	}
 }
