0ce62ff76cf6a2801fce78083c981138a600b641
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-14 16:48:13 +0200

    check and update entries when adding to ip network list
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/ipnetlist.go
+++ internal/cmd/ipnetlist.go
@@ -1,6 +1,8 @@
 package cmd
 
-import "net"
+import (
+	"net"
+)
 
 var (
 	allowedIPNets ipNetList
@@ -13,6 +15,31 @@ type ipNetList struct {
 
 // add adds ip network ipNet to the list
 func (i *ipNetList) add(ipNet *net.IPNet) {
+	// check if there is an existing entry that contains the new ip
+	// network, or if an existing ip network entry should be updated with
+	// the new one
+	for _, existing := range i.l {
+		// get netmask bits and lengths
+		eOnes, eBits := existing.Mask.Size()
+		nOnes, nBits := ipNet.Mask.Size()
+		if eBits != nBits {
+			// incompatible mask lengths, skip
+			continue
+		}
+		if existing.Contains(ipNet.IP) && eOnes < nOnes {
+			// existing entry already contains new one, stop
+			return
+		}
+		if ipNet.Contains(existing.IP) && eOnes > nOnes {
+			// new entry contains existing one, overwrite
+			// existing element and stop
+			existing.IP = ipNet.IP
+			existing.Mask = ipNet.Mask
+			return
+		}
+	}
+
+	// new element, add it
 	i.l = append(i.l, ipNet)
 }
 
