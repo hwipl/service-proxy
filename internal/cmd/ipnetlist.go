be655312a59c9834a134e161d3245927ac991158
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-14 17:56:31 +0200

    add merging multiple entries when adding to ip network list
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/ipnetlist.go
+++ internal/cmd/ipnetlist.go
@@ -16,28 +16,33 @@ type ipNetList struct {
 // add adds ip network ipNet to the list
 func (i *ipNetList) add(ipNet *net.IPNet) {
 	// check if there is an existing entry that contains the new ip
-	// network, or if an existing ip network entry should be updated with
+	// network, or if existing ip network entries should be replaced by
 	// the new one
+	k := 0
 	for _, existing := range i.l {
 		// get netmask bits and lengths
 		eOnes, eBits := existing.Mask.Size()
 		nOnes, nBits := ipNet.Mask.Size()
-		if eBits != nBits {
-			// incompatible mask lengths, skip
-			continue
+		if eBits == nBits {
+			if existing.Contains(ipNet.IP) && eOnes <= nOnes {
+				// existing entry already contains new one,
+				// stop here
+				return
+			}
+			if ipNet.Contains(existing.IP) && eOnes > nOnes {
+				// new entry contains existing one, remove
+				// existing entry and add new one later
+				continue
+			}
 		}
-		if existing.Contains(ipNet.IP) && eOnes <= nOnes {
-			// existing entry already contains new one, stop
-			return
-		}
-		if ipNet.Contains(existing.IP) && eOnes > nOnes {
-			// new entry contains existing one, overwrite
-			// existing element and stop
-			existing.IP = ipNet.IP
-			existing.Mask = ipNet.Mask
-			return
-		}
+
+		// keep entry in list
+		i.l[k] = existing
+		k++
+
 	}
+	// update list with remaining entries
+	i.l = i.l[:k]
 
 	// new element, add it
 	i.l = append(i.l, ipNet)
