67e36a9e961eb39f95cf566f75d36bceab32f217
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-14 15:36:52 +0200

    add internal/cmd/ipnetlist.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/ipnetlist.go
@@ -0,0 +1,23 @@
+package cmd
+
+import "net"
+
+// ipNetList is a list of IP network addresses
+type ipNetList struct {
+	l []*net.IPNet
+}
+
+// add adds ip network ipNet to the list
+func (i *ipNetList) add(ipNet *net.IPNet) {
+	i.l = append(i.l, ipNet)
+}
+
+// containsIP checks if any ip network in the list contains ip
+func (i *ipNetList) containsIP(ip net.IP) bool {
+	for _, ipNet := range i.l {
+		if ipNet.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
