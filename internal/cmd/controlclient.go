3764ceec995a83d446f23197372b7e2454a0d053
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-17 16:37:27 +0200

    send keep-alive messages every 15s in runClient()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/controlclient.go
+++ internal/cmd/controlclient.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 	"net"
+	"time"
 )
 
 // controlClient stores control client information
@@ -28,10 +29,7 @@ func (c *controlClient) runClient() {
 		log.Printf("Sending service registration %s to server", spec)
 		m := spec.toMessage()
 		tcpWriteToConn(c.conn, m.serialize())
-	}
 
-	// keep connection open
-	for {
 		// read reply messages from server
 		var msg message
 		buf := readFromConn(c.conn)
@@ -55,6 +53,23 @@ func (c *controlClient) runClient() {
 			// unknown message, stop here
 			log.Println("Unknown reply from server, " +
 				"closing connection")
+			return
+		}
+	}
+	// keep connection open
+	go func() {
+		for {
+			// send a keep-alive/NOP message every 15 seconds
+			time.Sleep(15 * time.Second)
+			keepAlive := message{Op: messageNop}
+			if !tcpWriteToConn(c.conn, keepAlive.serialize()) {
+				return
+			}
+		}
+	}()
+	for {
+		if readFromConn(c.conn) == nil {
+			log.Println("Closing connection to server")
 			return
 		}
 	}
