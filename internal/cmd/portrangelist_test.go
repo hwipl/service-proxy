604894d023def594a0391122253e9bcc2e078a4a
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-22 00:31:53 +0200

    add internal/cmd/portrangelist_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/portrangelist_test.go
@@ -0,0 +1,76 @@
+package cmd
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPortRangeListAdd(t *testing.T) {
+	var got, want []string
+	var ports portRangeList
+	var test = func() {
+		got = []string{}
+		for _, p := range ports.getAll() {
+			got = append(got, p.String())
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+
+	// add single ports
+	ports.add(protocolTCP, 1024, 1024)
+	ports.add(protocolUDP, 1024, 1024)
+
+	want = []string{
+		"tcp:1024-1024",
+		"udp:1024-1024",
+	}
+	test()
+
+	// add port ranges
+	ports = portRangeList{}
+	ports.add(protocolTCP, 1024, 65535)
+	ports.add(protocolUDP, 1024, 65535)
+
+	want = []string{
+		"tcp:1024-65535",
+		"udp:1024-65535",
+	}
+	test()
+
+	// add port ranges containing each other, size increasing
+	ports = portRangeList{}
+	ports.add(protocolTCP, 4096, 8192)
+	ports.add(protocolTCP, 2048, 16384)
+	ports.add(protocolTCP, 1024, 32768)
+
+	want = []string{
+		"tcp:1024-32768",
+	}
+	test()
+
+	// add port ranges containing each other, size decreasing
+	ports = portRangeList{}
+	ports.add(protocolTCP, 1024, 32768)
+	ports.add(protocolTCP, 2048, 16384)
+	ports.add(protocolTCP, 4096, 8192)
+
+	want = []string{
+		"tcp:1024-32768",
+	}
+	test()
+
+	// add overlapping port ranges
+	ports = portRangeList{}
+	ports.add(protocolTCP, 4096, 8192)
+	ports.add(protocolTCP, 16384, 32768)
+	ports.add(protocolTCP, 2048, 4096)
+	ports.add(protocolTCP, 8192, 16384)
+	ports.add(protocolTCP, 1024, 2048)
+
+	want = []string{
+		"tcp:1024-32768",
+	}
+	test()
+}
