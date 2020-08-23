43d40aa3a414b03c2376db7dbf70d83ce1f08b3c
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-23 20:15:40 +0200

    add internal/cmd/servicespec_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/servicespec_test.go
@@ -0,0 +1,66 @@
+package cmd
+
+import "testing"
+
+func TestServiceSpecToMessage(t *testing.T) {
+	s := serviceSpec{
+		protocol: "tcp",
+		port:     1024,
+		destPort: 1024,
+	}
+	want := message{
+		Op:       messageAdd,
+		Protocol: protocolTCP,
+		Port:     1024,
+		DestPort: 1024,
+	}
+	got := s.toMessage()
+	if *got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestServiceSpecFromMessage(t *testing.T) {
+	m := message{
+		Op:       messageAdd,
+		Protocol: protocolTCP,
+		Port:     1024,
+		DestPort: 1024,
+	}
+	want := serviceSpec{
+		protocol: "tcp",
+		port:     1024,
+		destPort: 1024,
+	}
+	got := serviceSpec{}
+	got.fromMessage(&m)
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestServiceSpecString(t *testing.T) {
+	s := serviceSpec{
+		protocol: "tcp",
+		port:     1024,
+		destPort: 1024,
+	}
+	want := "tcp:1024:1024"
+	got := s.String()
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestParseServiceSpec(t *testing.T) {
+	s := "tcp:1024:1024"
+	want := serviceSpec{
+		protocol: "tcp",
+		port:     1024,
+		destPort: 1024,
+	}
+	got := parseServiceSpec(s)
+	if *got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
