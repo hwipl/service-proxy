38f8b930fff07c5042971f618bb9dbf071ed623e
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-25 13:37:06 +0200

    add TestParseCertFiles()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd_test.go
+++ internal/cmd/cmd_test.go
@@ -1,6 +1,11 @@
 package cmd
 
-import "testing"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
 
 func TestParseTCPAddr(t *testing.T) {
 	parseAllowedIP("127.0.0.1")
@@ -44,4 +49,51 @@ func TestParseAllowedPort(t *testing.T) {
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
 	}
+}
+
+func TestParseCertFiles(t *testing.T) {
+	certPem := []byte(`-----BEGIN CERTIFICATE-----
+MIIBhTCCASugAwIBAgIQIRi6zePL6mKjOipn+dNuaTAKBggqhkjOPQQDAjASMRAw
+DgYDVQQKEwdBY21lIENvMB4XDTE3MTAyMDE5NDMwNloXDTE4MTAyMDE5NDMwNlow
+EjEQMA4GA1UEChMHQWNtZSBDbzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABD0d
+7VNhbWvZLWPuj/RtHFjvtJBEwOkhbN/BnnE8rnZR8+sbwnc/KhCk3FhnpHZnQz7B
+5aETbbIgmuvewdjvSBSjYzBhMA4GA1UdDwEB/wQEAwICpDATBgNVHSUEDDAKBggr
+BgEFBQcDATAPBgNVHRMBAf8EBTADAQH/MCkGA1UdEQQiMCCCDmxvY2FsaG9zdDo1
+NDUzgg4xMjcuMC4wLjE6NTQ1MzAKBggqhkjOPQQDAgNIADBFAiEA2zpJEPQyz6/l
+Wf86aX6PepsntZv2GYlA5UpabfT2EZICICpJ5h/iI+i341gBmLiAFQOyTDT+/wQc
+6MF9+Yw1Yy0t
+-----END CERTIFICATE-----`)
+	keyPem := []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIIrYSSNQFaA2Hwf1duRSxKtLYX5CB04fSeQ6tF1aY/PuoAoGCCqGSM49
+AwEHoUQDQgAEPR3tU2Fta9ktY+6P9G0cWO+0kETA6SFs38GecTyudlHz6xvCdz8q
+EKTcWGekdmdDPsHloRNtsiCa697B2O9IFA==
+-----END EC PRIVATE KEY-----`)
+
+	// create file function
+	createFile := func(name string, content []byte) *os.File {
+		tmpFile, err := ioutil.TempFile("", name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := tmpFile.Write(content); err != nil {
+			log.Fatal(err)
+		}
+		if err := tmpFile.Close(); err != nil {
+			log.Fatal(err)
+		}
+		return tmpFile
+	}
+
+	// create certificate file
+	cf := createFile("parsecertfilestest-cert-*.pem", certPem)
+	defer os.Remove(cf.Name())
+
+	// create key file
+	kf := createFile("parsecertfilestest-key-*.pem", keyPem)
+	defer os.Remove(kf.Name())
+
+	// test parsing
+	certFile = cf.Name()
+	keyFile = kf.Name()
+	parseCertFiles()
 }
